fe1c8a64f95a78bb3a375eaf5999b8620d4fca5d
sonu1239 <ss316742@gmail.com>
2021-10-28 12:55:37 +0530

    Edit Data to Contact List

--- Program.cs
+++ Program.cs
@@ -11,12 +11,55 @@ namespace AddressBook72
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to Address Book");
+            UC3AddressBook book = new UC3AddressBook(); // creating object of class
+            string yes = "y";
+            string y;
 
-            UC1AddressBook book = new UC1AddressBook(); // creating object of class
+            do
+            {
+                Console.WriteLine("Welcome to Address Book");
+                Console.WriteLine("1.AddNewContact\n2.ShowContact\n3.EditContact\n4.RmoveContact");
+                Console.WriteLine("\nEnter your choice");
+                int ch = Convert.ToInt32(Console.ReadLine());
 
-            book.GetContactDetails();   //through object accessing method 
-            book.putDetails();
+
+                switch (ch)
+                {
+
+                    case 1:
+                        Console.WriteLine("\nhow many contact you want to add");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < n; i++)
+                        {
+                            book.GetContactDetails();
+                            Console.WriteLine("\n");    //through object accessing method 
+
+                        }
+                        break;
+                    case 2:
+                        book.putDetails();
+                        break;
+
+                    case 3:
+                        /// Edit Contact
+                        book.editContact();
+                        break;
+
+                    case 4:
+                        /// Remove Contact
+                       // book.removeContact();
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.WriteLine("do you want to continue? press...y/n");
+                y = Console.ReadLine();
+
+            } while (yes == y);
+
+            Console.ReadLine();
+
         }
     }
 }
