32277f5c8fc33bbf31163259ede148688624bb2d
sonu1239 <ss316742@gmail.com>
2021-10-28 13:27:27 +0530

    Remove the Contact From List

---
+++ ContactList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook72
+{
+    class ContactList
+    {
+        public string firstName;
+        public string lastName;
+        public string[] address = new string[2];
+        public string state;
+        public int zipCode;
+        public long phoneNumber;
+        public string email;
+        
+        /// here created the parameter constructor of class ContactList and passing the value of person 
+        public ContactList(string firstName, string lastName, string[] address, string state, int zipCode, long phoneNumber, string email)
+        {
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.state = state;
+            this.zipCode = zipCode;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+
+        }
+    }
+}
