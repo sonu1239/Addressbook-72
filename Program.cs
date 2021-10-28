6d0daaf5d1fc68a17a7b9f8752ee3d0570ace340
sonu1239 <ss316742@gmail.com>
2021-10-28 12:12:58 +0530

    Addding data to contact list

--- Program.cs
+++ Program.cs
@@ -10,13 +10,54 @@ namespace AddressBook72
     {
         static void Main(string[] args)
         {
+            UC2AddressBook book = new UC2AddressBook(); // creating object of class
+            string yes = "y";
+            string y;
 
-            Console.WriteLine("Welcome to Address Book");
+            do
+            {
+                Console.WriteLine("Welcome to Address Book");
+                Console.WriteLine("1.AddNewContact\n2.ShowContact\n3.EditContact\n4.RmoveContact");
+                Console.WriteLine("\nEnter your choice");
+                int ch = Convert.ToInt32(Console.ReadLine());
 
-            UC1AddressBook book = new UC1AddressBook(); // creating object of class
 
-            book.GetContactDetails();   //through object accessing method 
-            book.putDetails();
+                switch (ch)
+                {
+
+                    case 1:
+                        Console.WriteLine("\nhow many contact you want to add");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < n; i++)
+                        {
+                            book.GetContactDetails();
+                            Console.WriteLine("\n");    //through object accessing method 
+
+                        }
+                        break;
+                    case 2:
+                        book.putDetails();
+                        break;
+
+                    case 3:
+                        /// Edit Contact
+                        break;
+
+                    case 4:
+                        /// Remove Contact
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.WriteLine("do you want to continue? press...y/n");
+                y = Console.ReadLine();
+
+            } while (yes == y);
+
+            Console.ReadLine();
+
         }
     }
+    
 }
